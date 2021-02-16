d47e46f4171c8c2e52dd2f81f9d6c58f35ad7cd4
Matthew Lau <lau.cy.matthew@gmail.com>
2021-02-16 16:49:58 +0000

    Updated Mongo password config in task def

--- config/db.js
+++ config/db.js
@@ -1,16 +1,15 @@
 const mongoose = require("mongoose");
 
+const mongoConnectionString = `mongodb+srv://admin-matlau:${process.env.MONGO_PASSWORD}@mattewcylau-5ltcp.mongodb.net/node-express-mongodb-docker?retryWrites=true`;
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      `mongodb+srv://admin-matlau:${process.env.MONGO_PASSWORD}@mattewcylau-5ltcp.mongodb.net/node-express-mongodb-docker?retryWrites=true`,
-      {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-      }
-    );
+    await mongoose.connect(mongoConnectionString, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    });
 
     console.log("MongoDB Connected...");
   } catch (err) {
